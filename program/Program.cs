4bfd7d4c183fa2ca1a07eee243589beada987b50
Alexandra Lisovskaya <lishura@rambler.ru>
2022-11-27 13:51:45 +0300

    add solving task

---
+++ program/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] array = new string[6] { "sasha", "98", "hirrr", "1234", "a", "marusya" };
+string[] secondarray = new string[array.Length];
+void FillSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+FillSecondArray(array, secondarray);
+PrintArray(secondarray);
